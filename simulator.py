767a9acc72540274e4648334b0f359bce691c1f2
AlekseiSandakov <axe455990@mail.ru>
2021-07-19 00:13:53 +0700

    My second commit

--- simulator.py
+++ simulator.py
@@ -1,11 +1,15 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
 from random import randint as ri
 
 
 def dice_roll():
-    '''Кидаем кость с сотней граней.
+    ''' Кидаем кость с сотней граней.
        Если значение бросока больше значения кости,
        то колесо будет цело.
-       Чем больше шанс прокола, тем меньше шанс успеха.'''
+       Чем больше шанс прокола, тем меньше шанс успеха. '''
     value = 50
     roll = ri(0, 100)
     if roll > value:
@@ -14,11 +18,13 @@ def dice_roll():
 
 
 class Track:
+    ''' Создаём Класс трэка. '''
     def __init__(self):
         self.distance = 100
 
 
 class Car(Track):
+    ''' Создаём Класс легкового автомобиля. '''
     def __init__(self):
         self.name = 'Car'
         self.speed = 120
@@ -33,6 +39,7 @@ class Car(Track):
 
 
 class Truck(Track):
+    ''' Создаём Класс грузового автомобиля. '''
     def __init__(self):
         self.name = 'Truck'
         self.speed = 100
@@ -47,6 +54,7 @@ class Truck(Track):
 
 
 class Bike(Track):
+    ''' Создаём Класс мотоцикла. '''
     def __init__(self):
         self.name = 'Bike'
         self.speed = 140
@@ -60,7 +68,8 @@ class Bike(Track):
             self.passing_distance_bike = int(track.distance / self.speed * 60)
 
 
-def debug_run():
+def run():
+    ''' Запуск события. '''
     car = Car()
     truck = Truck()
     bike = Bike()
@@ -75,16 +84,78 @@ def debug_run():
             if results[k] == i:
                 sorted_dict[k] = results[k]
                 break
-    first_place_name = tuple(sorted_dict.keys())[0]
-    first_place_passing_distance = tuple(sorted_dict.values())[0]
-    second_place_name = tuple(sorted_dict.keys())[1]
-    second_place_name_passing_distance = tuple(sorted_dict.values())[1]
-    third_place_name = tuple(sorted_dict.keys())[2]
-    third_place_passing_distance = tuple(sorted_dict.values())[2]
+    first_place = insertText (f'Первое место занял {tuple(sorted_dict.keys())[0]}, результат {tuple(sorted_dict.values())[0]} МИН!')
+    second_place = insertText (f'Второе место занял {tuple(sorted_dict.keys())[1]}, результат {tuple(sorted_dict.values())[1]} МИН!')
+    third_place = insertText (f'Третье место занял {tuple(sorted_dict.keys())[2]}, результат {tuple(sorted_dict.values())[2]} МИН!')
 
-    print(f'Первое место занял {first_place_name}, результат {first_place_passing_distance} МИН!')
-    print(f'Второе место занял {second_place_name}, результат {second_place_name_passing_distance} МИН!')
-    print(f'Третье место занял {third_place_name}, результат {third_place_passing_distance} МИН!')
+    return first_place, second_place, third_place
 
 
-debug_run()
+root = Tk()
+
+''' Размеры окна программы. '''
+WIDTH = 1024
+HEIGHT = 600
+
+''' Создаём главное окно
+Вычисляем координаты для размещения окна по центру. '''
+POS_X = root.winfo_screenwidth() // 2 - WIDTH // 2
+POS_Y = root.winfo_screenwidth() // 2 - HEIGHT // 2
+
+''' Установка заголовка. '''
+root.title('ГОНКИ ПО КРУГУ')
+
+''' Запрещаем изменение размеров. '''
+root.resizable(False, False)
+
+''' Устанавливаем ширину, высоту и позицию. '''
+root.geometry(f'{WIDTH}x{HEIGHT}+{POS_X}+{POS_Y}')
+
+''' Загружаем изображение фона. '''
+road_image = PhotoImage(file='road.png')
+
+''' Устанавливаем изображение в Label. '''
+road = Label(root, image=road_image)
+road.place(x=0, y=17)
+
+''' Создаём кнопку и выводим её на экран. '''
+startButton = Button(text='СТАРТ',
+                     font='arial 20',
+                     width=61,
+                     background='#37AA37')
+startButton.place(x=20, y=370)
+startButton['state'] = 'normal'
+
+''' Создаём информационный чат виджетом Text. '''
+textDiary = Text(width=70, height=8, wrap=WORD)
+textDiary.place(x=430, y=450)
+
+''' Создаём и прикрепляем к тексту полосу прокрутки. '''
+scroll = Scrollbar(command=textDiary.yview, width=20)
+scroll.place(x=990, y=450, height=132)
+textDiary['yscrollcommand'] = scroll.set
+
+
+def championship_table():
+    ''' Отображение результатов. '''
+    first_place = 0
+    second_place = 0
+    third_place = 0
+    insertText(first_place)
+    insertText(second_place)
+    insertText(third_place)
+
+
+def insertText(run):
+    ''' Добавление строки в текстовый блок. '''
+    textDiary.insert(INSERT, run)
+    textDiary.see(END)
+
+
+''' Назначаем метод, выполняющийся при нажатии на 'Старт'. '''
+startButton['command'] = run
+
+championship_table()
+
+''' Выводим главное окно на экран. '''
+root.mainloop()
